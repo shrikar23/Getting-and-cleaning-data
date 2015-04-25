ef53b494e34ed276d2c0b4d9905fe3a974a845db
Shrikar Umarji <shrikar23@gmail.com>
2015-04-25 17:14:43 +0530

    R script

---
+++ run_analysis.R
@@ -0,0 +1,77 @@
+##-------------------------------------STEP 1----------------------------------
+# install and load the reshape2 package.
+# Uncomment the below two lines if you need to install and load the reshape2 package.
+#   install.packages("reshape2")
+    library("reshape2")
+
+##-------------------------------------STEP 2----------------------------------
+#Declare the column variables to be used. 
+ColumnsToSelect <- c(1,2,3,4,5,6,41,42,43,44,45,46,81,82,83,84,85,86,121,122,123,124,125,126,161,162,163,164,165,166,201,202,214,215,227,228,240,241,253,254,266,267,268,269,270,271,345,346,347,348,349,350,424,425,426,427,428,429,503,504,516,517,529,530,542,543)
+ColumnNames <- c("tBodyAcc-mean()-X","tBodyAcc-mean()-Y","tBodyAcc-mean()-Z","tBodyAcc-std()-X","tBodyAcc-std()-Y","tBodyAcc-std()-Z","tGravityAcc-mean()-X","tGravityAcc-mean()-Y","tGravityAcc-mean()-Z","tGravityAcc-std()-X","tGravityAcc-std()-Y","tGravityAcc-std()-Z","tBodyAccJerk-mean()-X","tBodyAccJerk-mean()-Y","tBodyAccJerk-mean()-Z","tBodyAccJerk-std()-X","tBodyAccJerk-std()-Y","tBodyAccJerk-std()-Z","tBodyGyro-mean()-X","tBodyGyro-mean()-Y","tBodyGyro-mean()-Z","tBodyGyro-std()-X","tBodyGyro-std()-Y","tBodyGyro-std()-Z","tBodyGyroJerk-mean()-X","tBodyGyroJerk-mean()-Y","tBodyGyroJerk-mean()-Z","tBodyGyroJerk-std()-X","tBodyGyroJerk-std()-Y","tBodyGyroJerk-std()-Z","tBodyAccMag-mean()","tBodyAccMag-std()","tGravityAccMag-mean()","tGravityAccMag-std()","tBodyAccJerkMag-mean()","tBodyAccJerkMag-std()","tBodyGyroMag-mean()","tBodyGyroMag-std()","tBodyGyroJerkMag-mean()","tBodyGyroJerkMag-std()","fBodyAcc-mean()-X","fBodyAcc-mean()-Y","fBodyAcc-mean()-Z","fBodyAcc-std()-X","fBodyAcc-std()-Y","fBodyAcc-std()-Z","fBodyAccJerk-mean()-X","fBodyAccJerk-mean()-Y","fBodyAccJerk-mean()-Z","fBodyAccJerk-std()-X","fBodyAccJerk-std()-Y","fBodyAccJerk-std()-Z","fBodyGyro-mean()-X","fBodyGyro-mean()-Y","fBodyGyro-mean()-Z","fBodyGyro-std()-X","fBodyGyro-std()-Y","fBodyGyro-std()-Z","fBodyAccMag-mean()","fBodyAccMag-std()","fBodyBodyAccJerkMag-mean()","fBodyBodyAccJerkMag-std()","fBodyBodyGyroMag-mean()","fBodyBodyGyroMag-std()","fBodyBodyGyroJerkMag-mean()","fBodyBodyGyroJerkMag-std()")
+ActivityCode <- c(1,2,3,4,5,6)
+ActivityNames <- c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING")
+
+
+run_analysis <- function() {
+##-------------------------------------STEP 3----------------------------------
+#creating clean data file for the test data set
+  
+  print ("Getting the data from test data set")
+  #Create a clean dataset from the needed columns of the test dataset. 
+  RawTestData <- read.table("UCI HAR Dataset/test/X_test.txt", sep = "")[,ColumnsToSelect]
+  colnames(RawTestData) <- ColumnNames
+  CleanTestData <- RawTestData
+  
+  #Create and merge the activity codes dataset with clean data set.
+  ActivityTestData <- read.table("UCI HAR Dataset/test/Y_test.txt", sep = "",col.names = "activity")
+  CleanTestData <- cbind(CleanTestData,ActivityTestData)
+  
+  #Create and Merge the subject dataset with clean dataset.
+  SubjectTestData <- read.table("UCI HAR dataset/test/subject_test.txt", sep = "", col.names = "subject")
+  CleanTestData <- cbind(CleanTestData, SubjectTestData)
+  
+  #Convert the activity column into a factor.
+  CleanTestData$activity <- factor(CleanTestData$activity, levels = ActivityCode, labels = ActivityNames)
+  
+  print("data fetched from test data")
+##-------------------------------------STEP 4----------------------------------
+#creating clean data file for the train data set
+  
+  print("Getting data from train data set")
+  #Create a clean dataset from the needed columns of the train dataset.
+  RawTrainData <- read.table("UCI HAR Dataset/train/X_train.txt", sep = "")[,ColumnsToSelect]
+  colnames(RawTrainData) <- ColumnNames
+  CleanTrainData <- RawTrainData
+  
+  #Create and merge the activity codes dataset with clean data set.
+  ActivityTrainData <- read.table("UCI HAR Dataset/train/Y_train.txt", sep = "",col.names = "activity")
+  CleanTrainData <- cbind(CleanTrainData, ActivityTrainData)
+  
+  #Create and Merge the subject dataset with clean dataset.
+  SubjectTrainData <- read.table("UCI HAR Dataset/train/subject_train.txt", sep = "", col.names = "subject")
+  CleanTrainData <- cbind(CleanTrainData, SubjectTrainData)
+  
+  #Convert the activity column into a factor.
+  CleanTrainData$activity <- factor(CleanTrainData$activity, levels = ActivityCode, labels = ActivityNames)
+  
+  print("data fetched from train data set")
+##-------------------------------------STEP 5---------------------------------- 
+
+  print("Processing...")
+  #Merging the the two clean datasets obtained from test and train datasets.
+  MergeData <- rbind(CleanTrainData, CleanTestData)
+  
+  #Obtaining average of each variable for each activity and each subject.   
+  MeltedData <- melt(MergeData, id = c("activity","subject"))
+  CleanData <- dcast(MeltedData, activity + subject ~ variable,mean)
+  
+  ##-------------------------------------STEP 6----------------------------------
+  
+  print("Writing the final data set...")
+  #Write the final tide data set to the file TideDataset.txt.
+  write.table(CleanData, "UCI HAR Dataset/TideDataset.txt", row.name = FALSE)
+  print("write complete")
+}
+
+#Run the function run_analysis.R
+#run_analysis()
