cdb3f82d65185215349f393348365e2580e08898
shrikar23 <shrikar23@gmail.com>
2015-04-25 17:20:07 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -4,7 +4,7 @@
 #   install.packages("reshape2")
     library("reshape2")
 
-##-------------------------------------STEP 2----------------------------------
+##------------------------------------------------------------------------------
 #Declare the column variables to be used. 
 ColumnsToSelect <- c(1,2,3,4,5,6,41,42,43,44,45,46,81,82,83,84,85,86,121,122,123,124,125,126,161,162,163,164,165,166,201,202,214,215,227,228,240,241,253,254,266,267,268,269,270,271,345,346,347,348,349,350,424,425,426,427,428,429,503,504,516,517,529,530,542,543)
 ColumnNames <- c("tBodyAcc-mean()-X","tBodyAcc-mean()-Y","tBodyAcc-mean()-Z","tBodyAcc-std()-X","tBodyAcc-std()-Y","tBodyAcc-std()-Z","tGravityAcc-mean()-X","tGravityAcc-mean()-Y","tGravityAcc-mean()-Z","tGravityAcc-std()-X","tGravityAcc-std()-Y","tGravityAcc-std()-Z","tBodyAccJerk-mean()-X","tBodyAccJerk-mean()-Y","tBodyAccJerk-mean()-Z","tBodyAccJerk-std()-X","tBodyAccJerk-std()-Y","tBodyAccJerk-std()-Z","tBodyGyro-mean()-X","tBodyGyro-mean()-Y","tBodyGyro-mean()-Z","tBodyGyro-std()-X","tBodyGyro-std()-Y","tBodyGyro-std()-Z","tBodyGyroJerk-mean()-X","tBodyGyroJerk-mean()-Y","tBodyGyroJerk-mean()-Z","tBodyGyroJerk-std()-X","tBodyGyroJerk-std()-Y","tBodyGyroJerk-std()-Z","tBodyAccMag-mean()","tBodyAccMag-std()","tGravityAccMag-mean()","tGravityAccMag-std()","tBodyAccJerkMag-mean()","tBodyAccJerkMag-std()","tBodyGyroMag-mean()","tBodyGyroMag-std()","tBodyGyroJerkMag-mean()","tBodyGyroJerkMag-std()","fBodyAcc-mean()-X","fBodyAcc-mean()-Y","fBodyAcc-mean()-Z","fBodyAcc-std()-X","fBodyAcc-std()-Y","fBodyAcc-std()-Z","fBodyAccJerk-mean()-X","fBodyAccJerk-mean()-Y","fBodyAccJerk-mean()-Z","fBodyAccJerk-std()-X","fBodyAccJerk-std()-Y","fBodyAccJerk-std()-Z","fBodyGyro-mean()-X","fBodyGyro-mean()-Y","fBodyGyro-mean()-Z","fBodyGyro-std()-X","fBodyGyro-std()-Y","fBodyGyro-std()-Z","fBodyAccMag-mean()","fBodyAccMag-std()","fBodyBodyAccJerkMag-mean()","fBodyBodyAccJerkMag-std()","fBodyBodyGyroMag-mean()","fBodyBodyGyroMag-std()","fBodyBodyGyroJerkMag-mean()","fBodyBodyGyroJerkMag-std()")
@@ -13,7 +13,8 @@ ActivityNames <- c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","
 
 
 run_analysis <- function() {
-##-------------------------------------STEP 3----------------------------------
+
+##-------------------------------------STEP 2----------------------------------
 #creating clean data file for the test data set
   
   print ("Getting the data from test data set")
@@ -34,7 +35,8 @@ run_analysis <- function() {
   CleanTestData$activity <- factor(CleanTestData$activity, levels = ActivityCode, labels = ActivityNames)
   
   print("data fetched from test data")
-##-------------------------------------STEP 4----------------------------------
+
+##-------------------------------------STEP 3----------------------------------
 #creating clean data file for the train data set
   
   print("Getting data from train data set")
@@ -55,18 +57,18 @@ run_analysis <- function() {
   CleanTrainData$activity <- factor(CleanTrainData$activity, levels = ActivityCode, labels = ActivityNames)
   
   print("data fetched from train data set")
-##-------------------------------------STEP 5---------------------------------- 
-
+  
+##-------------------------------------STEP 4---------------------------------- 
   print("Processing...")
   #Merging the the two clean datasets obtained from test and train datasets.
   MergeData <- rbind(CleanTrainData, CleanTestData)
   
+##------------------------------------STEP 5------------------------------------  
   #Obtaining average of each variable for each activity and each subject.   
   MeltedData <- melt(MergeData, id = c("activity","subject"))
   CleanData <- dcast(MeltedData, activity + subject ~ variable,mean)
   
   ##-------------------------------------STEP 6----------------------------------
-  
   print("Writing the final data set...")
   #Write the final tide data set to the file TideDataset.txt.
   write.table(CleanData, "UCI HAR Dataset/TideDataset.txt", row.name = FALSE)
